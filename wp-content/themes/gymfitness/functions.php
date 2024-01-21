0651e82f7e205efb511d5f5f936ed6048ab42875
oxicuza <oxicusa@gmail.com>
2024-01-22 04:56:59 +0700

    adding google fonts

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -15,5 +15,8 @@ add_action('init', 'gymfitness_menus');
 function gymfitness_scripts() {
     // Normalize css
     wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css');
+
+    // Google fonts
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Raleway:ital,wght@0,100..900;1,100..900&family=Staatliches&display=swap');
 }
 add_action('wp_enqueue_scripts', 'gymfitness_scripts');
