d28f5109cced20391d53973e9d6cf39e27bf2564
oxicuza <oxicusa@gmail.com>
2024-01-22 05:05:35 +0700

    adding main stylesheet

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -18,5 +18,8 @@ function gymfitness_scripts() {
 
     // Google fonts
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Raleway:ital,wght@0,100..900;1,100..900&family=Staatliches&display=swap');
+
+    // Main stylesheet
+    wp_enqueue_style('main-stylesheet', get_stylesheet_uri(), array('normalize', 'google-fonts'), '1.0.0');
 }
 add_action('wp_enqueue_scripts', 'gymfitness_scripts');
