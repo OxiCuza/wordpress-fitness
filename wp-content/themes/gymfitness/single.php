7f44c95ee2a0cf40748731f226e0a0229bef1b5d
oxicuza <oxicusa@gmail.com>
2024-01-22 16:12:09 +0700

    add style for footer

--- wp-content/themes/gymfitness/single.php
+++ wp-content/themes/gymfitness/single.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
 
-<?php while(have_posts()): the_post(); ?>
+    <?php while(have_posts()): the_post(); ?>
 
-    <h1><?php the_title(); ?></h1>
+        <h1><?php the_title(); ?></h1>
 
-    <?php the_content(); ?>
-    
-<?php endwhile; ?>
+        <?php the_content(); ?>
+        
+    <?php endwhile; ?>
+
+<?php get_footer(); ?>
